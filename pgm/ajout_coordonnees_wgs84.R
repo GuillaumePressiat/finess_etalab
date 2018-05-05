26e22a1aba64769ed4ad43e0b79a763baf3f5efb
GuillaumePressiat <guillaume.pressiat@aphp.fr>
2018-05-05 11:55:44 +0200

    projection Lambert 93 > epsg 2154

--- pgm/ajout_coordonnees_wgs84.R
+++ pgm/ajout_coordonnees_wgs84.R
@@ -6,7 +6,8 @@ convertir_proj <- function(p){
   
   if (p != ""){
   convert_string <- case_when(
-   p == 'LAMBERT_93' ~ "+proj=lcc +lat_1=44 +lat_2=49+lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +units=m +no_defs",
+   #p == 'LAMBERT_93' ~ "+proj=lcc +lat_1=44 +lat_2=49+lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +towgs84=0,0,0,0,0,0,0 +ellps=GRS80 +units=m +no_defs",
+   p == 'LAMBERT_93' ~ "+init=epsg:2154",
    p == 'UTM_N20'    ~ "+proj=utm +zone=20 +datum=WGS84",
    p == 'UTM_N21'    ~ "+proj=utm +zone=21 +datum=WGS84",
    p == 'UTM_N22'    ~ "+proj=utm +zone=22 +datum=WGS84",
